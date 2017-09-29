e607f73ab601d7a2373f9bf02fb67ef452777754
kolliparacs <subhash.kollipara@gmail.com>
2017-09-28 21:28:42 -0500

    Minor changes and code clean up

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 $(function() {
+	// Initial list of tasks
 	var taskList = [
-		{"name":"Test Task #1","date":"12/01/2012","assigned":"John Doe"},
-		{"name":"Test Task #2","date":"12/02/2012","assigned":"John Doe"},
-		{"name":"Test Task #3","date":"12/03/2012","assigned":"John Doe"},
-		{"name":"Test Task #4","date":"12/04/2012","assigned":"John Doe"},
-		{"name":"Test Task #5","date":"12/05/2012","assigned":"John Doe"},
-		{"name":"Test Task #6","date":"12/06/2012","assigned":"John Doe"},
-		{"name":"Test Task #7","date":"12/07/2012","assigned":"John Doe"}
+		{"name":"Test Task #1","date":"2012-12-01","assigned":"John Doe"},
+		{"name":"Test Task #2","date":"2012-12-02","assigned":"John Doe"},
+		{"name":"Test Task #3","date":"2012-12-03","assigned":"John Doe"},
+		{"name":"Test Task #4","date":"2012-12-04","assigned":"John Doe"},
+		{"name":"Test Task #5","date":"2012-12-05","assigned":"John Doe"},
+		{"name":"Test Task #6","date":"2012-12-06","assigned":"John Doe"},
+		{"name":"Test Task #7","date":"2012-12-07","assigned":"John Doe"}
 	];
 	var table = $('#task-table tbody')[0];
 	var columnCount = Object.keys(taskList[0]).length;
+	//Insert rows for each task in the list and append to existing table
 	for (var i = 0; i < taskList.length; i++) {
         row = table.insertRow(-1);
         for (key in taskList[i]) {
@@ -23,6 +25,7 @@ $(function() {
 		  console.log(data);
 	  }
 	});*/
+	//Insert the form data as a new row into the table
 	$('#add-task').off('click').on('click', function(){
 		var table = $('#task-table tbody')[0];
 		row = table.insertRow(-1);
